255c8a7e3792da9c91ff961aedaefb81b4da6a29
Alana Mello <alana.f.mello@hotmail.com>
2019-04-12 17:26:06 -0300

    Add Good morning

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,9 @@ namespace ConsoleApp1 {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello world!");
+            Console.WriteLine("Good morning!");
+
+            Console.ReadLine();
         }
     }
 }
